4977c1465671dc8944b7e464aa09a07c2f4bec91
SonNV MacMini <sonnv>
2021-07-22 18:26:22 +0700

    add file

---
+++ MultipleFunction/Pods/Target Support Files/Pods-MultipleFunction-MultipleFunctionUITests/Pods-MultipleFunction-MultipleFunctionUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MultipleFunction_MultipleFunctionUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MultipleFunction_MultipleFunctionUITests
+@end
---
+++ MultipleFunction/Pods/Target Support Files/Pods-MultipleFunctionTests/Pods-MultipleFunctionTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MultipleFunctionTests : NSObject
+@end
+@implementation PodsDummy_Pods_MultipleFunctionTests
+@end
